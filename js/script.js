29d23db83124c205fd9777894971b64f217db7a7
James Ocana <jamesocana@macbook-air.myfiosgateway.com>
2022-08-22 13:46:57 -0400

    second API connected

--- js/script.js
+++ js/script.js
@@ -4,7 +4,10 @@
 //let URL = "https://zenquotes.io/api/quotes"
 //$.ajax("https://zenquotes.io/api/quotes")
 
-let URL = "https://random-interview.herokuapp.com/question/random"
+let URL2 = "https://quotes.rest/qod.json?language=en" //CONNECTED! 
+//https://quotes.rest/quote/random?language=en
+
+let URL = "https://random-interview.herokuapp.com/question/random" //CONNECTED!
 
 //CONSTANTS//
 
@@ -20,10 +23,19 @@ let URL = "https://random-interview.herokuapp.com/question/random"
 
 
 
-$.ajax(URL).then(function(data) {
-    $('p').text(data.question)
-    console.log(data.question)
-    console.log(URL)
+// $.ajax(URL).then(function(data) {
+//     $('p').text(data.question)
+//     console.log(data.question)
+//     console.log(URL)
+// },function(error) {
+//     console.log("error")
+// });
+
+$.ajax(URL2).then(function(data) {
+    $('p').text(data.contents.quotes[0].quote + " - " + data.contents.quotes[0].author)
+    console.log(data.contents)
+    console.log(data.contents.quotes[0].quote)
+    console.log(URL2)
 },function(error) {
     console.log("error")
 });
