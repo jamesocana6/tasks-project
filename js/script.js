0226601fd576c438842cc769ab05f9811957279f
James Ocana <jamesocana@macbook-air.myfiosgateway.com>
2022-08-22 11:32:46 -0400

    connected one API and prints it on screen

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
-alert('hello')
+
+
+//URL: https://zenquotes.io/api/random
+//let URL = "https://zenquotes.io/api/quotes"
+//$.ajax("https://zenquotes.io/api/quotes")
+
+let URL = "https://random-interview.herokuapp.com/question/random"
+
+//CONSTANTS//
+
+
+//ELEMENTS//
+
+
+//ONCLICK EVENTS//
+
+
+//FUNCTIONS//
+
+
+
+
+$.ajax(URL).then(function(data) {
+    $('p').text(data.question)
+    console.log(data.question)
+    console.log(URL)
+},function(error) {
+    console.log("error")
+});
