c1632bdd1f4dd800b94824912ae1431dbf70da1f
James Ocana <jamesocana@macbook-air.myfiosgateway.com>
2022-08-22 14:58:56 -0400

    replaced and connected third API

--- js/script.js
+++ js/script.js
@@ -1,11 +1,7 @@
 
-
-//URL: https://zenquotes.io/api/random
-//let URL = "https://zenquotes.io/api/quotes"
-//$.ajax("https://zenquotes.io/api/quotes")
+let URL3 = "https://api.quotable.io/random"
 
 let URL2 = "https://quotes.rest/qod.json?language=en" //CONNECTED! 
-//https://quotes.rest/quote/random?language=en
 
 let URL = "https://random-interview.herokuapp.com/question/random" //CONNECTED!
 
@@ -13,7 +9,7 @@ let URL = "https://random-interview.herokuapp.com/question/random" //CONNECTED!
 
 
 //ELEMENTS//
-
+let $list = $('')
 
 //ONCLICK EVENTS//
 
@@ -22,7 +18,9 @@ let URL = "https://random-interview.herokuapp.com/question/random" //CONNECTED!
 
 
 
-
+////////////////////////////////////////
+//APIs for quotes are connected//
+////////////////////////////////////////
 // $.ajax(URL).then(function(data) {
 //     $('p').text(data.question)
 //     console.log(data.question)
@@ -31,11 +29,51 @@ let URL = "https://random-interview.herokuapp.com/question/random" //CONNECTED!
 //     console.log("error")
 // });
 
-$.ajax(URL2).then(function(data) {
-    $('p').text(data.contents.quotes[0].quote + " - " + data.contents.quotes[0].author)
-    console.log(data.contents)
-    console.log(data.contents.quotes[0].quote)
-    console.log(URL2)
-},function(error) {
-    console.log("error")
-});
+// $.ajax(URL2).then(function(data) {
+//     $('p').text(data.contents.quotes[0].quote + " - " + data.contents.quotes[0].author)
+//     console.log(data.contents)
+//     console.log(data.contents.quotes[0].quote)
+//     console.log(URL2)
+// },function(error) {
+//     console.log("error")
+// });
+
+// $.ajax(URL3).then(function (data) {
+//     $('p').text(data.content + " - " + data.author)
+//     console.log(data)
+//     console.log(data.content)
+//     console.log(URL3)
+// }, function (error) {
+//     console.log("error")
+// });
+
+
+
+/////////////////////////////////////
+///FIGURE OUT HOW TO GET AROUND CORS OR FIND NEW API?
+/////////////////////////////////////
+
+// const api_url ="https://zenquotes.io/api/quotes/";
+
+// async function getapi(url)
+// {
+//   const response = await fetch(url);
+//   var data = await response.json();
+//   console.log(data);
+// }
+
+// getapi(api_url);
+
+// let headers = new Headers();
+
+//   headers.append('Content-Type', 'application/json');
+//   headers.append('Accept', 'application/json');
+
+// let xhr = new XMLHttpRequest();
+// xhr.open('POST', 'https://zenquotes.io/api/random/');
+// xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
+// xhr.setRequestHeader('Content-type','application/json');
+
+//URL: https://zenquotes.io/api/random
+// let URL3 = "https://zenquotes.io/api/random/"
+//$.ajax("https://zenquotes.io/api/quotes")
