cfd6e87e9d9e1d79c3db4a0f97dd2f779309c918
James Ocana <jamesocana@macbook-air.myfiosgateway.com>
2022-08-24 14:34:56 -0400

    changed styling

--- js/home.js
+++ js/home.js
@@ -2,13 +2,17 @@ populateList(savedList, savedListStr);
 setUpQuote();
 
 $("tbody > tr").hover(function(evt) {
-    $(evt.target).closest("tr").css("backgroundColor", "darkolivegreen");
+    $(evt.target).closest("tr").css("backgroundColor", "lightgreen");
     //target the edit and remove buttons
     $(evt.target).closest("tr").children().eq(1).children().eq(0).css("display", "table-cell");
     $(evt.target).closest("tr").children().eq(2).children().eq(0).css("display", "table-cell");
 },function(evt) {
-    $(evt.target).closest("tr").css("backgroundColor", "green");
+    $(evt.target).closest("tr").css("backgroundColor", "");
     //target the edit and remove buttons
     $(evt.target).closest("tr").children().eq(1).children().eq(0).css("display", "none");
     $(evt.target).closest("tr").children().eq(2).children().eq(0).css("display", "none");
 });
+
+$("div#date").append(`<p id="month">${month}</p>`);
+$("div#date").append(`<p id="day">${day}</p>`);
+$("div#date").append(`<p id="year">${year}</p>`);
